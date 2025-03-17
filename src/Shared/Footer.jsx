4fd30a5caf0e146270cd48e644fd151fc6316f23
tainur <tainurrahaman31@gmail.com>
2025-03-17 13:42:55 +0600

    customize the footer and responsive it for all devices

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,15 +1,14 @@
 import { FaTwitter, FaYoutube } from "react-icons/fa";
 import logo from "../assets/logo.png";
 import { SiFacebook } from "react-icons/si";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-800 text-neutral-content px-10 py-5 md:py-10 md:mt-14 lg:mt-20">
+    <footer className="footer footer-horizontal footer-center bg-slate-800 text-neutral-content rounded p-10">
       <div className="flex flex-col md:flex-row justify-between md:justify-evenly gap-5 md:gap-10 w-full">
-        {" "}
-        <aside className="text-center md:text-start">
+        <div className="text-center md:text-start">
           <div className="flex justify-center md:justify-start">
-            {" "}
             <img
               src={logo}
               className="w-10 md:w-14 lg:w-16"
@@ -17,45 +16,48 @@ const Footer = () => {
             />
           </div>
           <p className="font-semibold text-[16px] pt-3">
-            ScholarBeacon NinetyOne.com
-            <br />
+            ScholarBeacon NinetyOne.com <br />
             Providing scholarship services since 1992
           </p>
-        </aside>
+        </div>
         <div className="flex gap-10 md:gap-14 lg:gap-20 justify-between">
-          <div className="flex gap-10 md:gap-14 lg:gap-20 justify-between">
-            <nav className="flex flex-col">
-              <h6 className="footer-title">Services</h6>
-              <a className="link link-hover">Branding</a>
-              <a className="link link-hover">Design</a>
-              <a className="link link-hover">Marketing</a>
-              <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav className="flex flex-col">
-              <h6 className="footer-title">Company</h6>
-              <a className="link link-hover">About us</a>
-              <a className="link link-hover">Contact</a>
-              <a className="link link-hover">Jobs</a>
-              <a className="link link-hover">Press kit</a>
-            </nav>
-          </div>
-          <nav>
+          <nav className="flex flex-col text-start">
+            <h6 className="footer-title">Company</h6>
+            <NavLink to="/">About Us</NavLink>
+            <NavLink to="/">Contact</NavLink>
+            <NavLink to="/">Jobs</NavLink>
+            <NavLink to="/">Press kit</NavLink>
+          </nav>
+          <nav className="text-start">
             <h6 className="footer-title">Social</h6>
-            <div className="grid grid-flow-row md:grid-flow-col gap-2 md:gap-4">
-              <a>
+            <div className="flex gap-2 md:gap-4">
+              <a
+                href="https://en.wikipedia.org/wiki/Twitter"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaTwitter className="text-2xl"></FaTwitter>
               </a>
-              <a>
+
+              <a
+                href="https://en.wikipedia.org/wiki/YouTube"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaYoutube className="text-2xl"></FaYoutube>
               </a>
-              <a>
-                <SiFacebook className="text-2xl"></SiFacebook>
+              <a
+                href="https://en.wikipedia.org/wiki/Facebook"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SiFacebook className="text-2xl" />
               </a>
             </div>
           </nav>
         </div>
       </div>
-      <aside className="w-full flex justify-center pt-4 md:pt-8">
+      <aside className="w-full flex justify-center pt-0 md:pt-8">
         <p className="font-medium text-[14px] md:text-[16px] text-center">
           Copyright © {new Date().getFullYear()} - All right reserved by
           ScholarBeacon NinetyOne.com
